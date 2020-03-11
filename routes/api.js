fbf8ae784d422695be630e45ec79ae02ccc884f2
NicholeSteig <nichole.steig@gmail.com>
2020-03-10 17:43:03 -0700

    rearranged stuff

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
+
+module.exports = router;
